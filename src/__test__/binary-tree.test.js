74e51c83ca0098b0feb3e6c6be6bf8f912a2eca2
Tracy Williams <selpilot@gmail.com>
2018-06-29 14:17:05 -0700

    initial lab-15 commit

---
+++ src/__test__/binary-tree.test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+import binaryTree from '../model/binary-tree';
+import { preOrderTraversal, inOrderTraversal, postOrderTraversal } from '../lib/traversals';
+
+describe('PRE-ORDER', () => {
+  test('Expecting a string of visited nodes as 1 2 6 7 8 9 3 4 5', () => {
+    // remember that I made my traversal signatures accept a callback so I can apply any kind of logic to each visited node in the test environment
+
+    let str = '';
+    preOrderTraversal(binaryTree.root, (nodeValue) => {
+      str += `${nodeValue} `;
+    });
+    expect(str.trim()).toEqual('1 2 6 7 8 9 3 4 5');
+  });
+});
+
+describe('POST-ORDER', () => {
+  test('Expecting a string of visited nodes as 8 9 7 6 2 4 5 3 1', () => {
+    // remember that I made my traversal signatures accept a callback so I can apply any kind of logic to each visited node in the test environment
+
+    let str = '';
+    postOrderTraversal(binaryTree.root, (nodeValue) => {
+      str += `${nodeValue} `;
+    });
+    expect(str.trim()).toEqual('8 9 7 6 2 4 5 3 1');
+  });
+});
+
+describe('IN-ORDER', () => {
+  test('Expecting a string of visited nodes as 8 9 7 6 2 1 4 5 3', () => {
+    // remember that I made my traversal signatures accept a callback so I can apply any kind of logic to each visited node in the test environment
+
+    let str = '';
+    inOrderTraversal(binaryTree.root, (nodeValue) => {
+      str += `${nodeValue} `;
+    });
+    expect(str.trim()).toEqual('8 9 7 6 2 1 4 5 3');
+  });
+});
